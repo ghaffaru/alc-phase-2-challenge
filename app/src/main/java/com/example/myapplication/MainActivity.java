b83f4e4086cef5ffa68d17e86c7ee118d69b5c1b
Ghaffaru <mudashiruagm@gmail.com>
2019-08-02 23:28:21 +0000

    setup firebase and first activity

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    EditText txtTitle;
+    EditText txtPrice;
+    EditText txtDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("traveldeals");
+
+        txtTitle = findViewById(R.id.txtTitle);
+        txtPrice = findViewById(R.id.txtPrice);
+        txtDescription = findViewById(R.id.txtDescription);
+
+
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.save_deal) {
+            saveDeal();
+            Toast.makeText(this, "Deal saved", Toast.LENGTH_LONG).show();
+            txtTitle.getText().clear();
+            txtPrice.getText().clear();
+            txtDescription.getText().clear();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void saveDeal() {
+        String title = txtTitle.getText().toString();
+        String price = txtPrice.getText().toString();
+        String description = txtDescription.getText().toString();
+
+        TravelDeal deal = new TravelDeal(title,price,description,"");
+        databaseReference.push().setValue(deal);
+
+        txtTitle.setText("");
+        txtPrice.setText("");
+        txtDescription.setText("");
+    }
+}
